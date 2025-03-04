9d5622f324c1dff57137bacfd0ad1734a7274dd1
Rajesh Jha <rajeshjha@Rajeshs-MacBook-Air.local>
2025-03-04 23:52:26 +0530

    feat: Add leetcode problem best time to buy and sell stock

---
+++ Arrays/Leetcode_121/bestTimeToBuyAndSell.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+var maxProfit = function(prices) {
+    let profit = 0;
+    let minimum = prices[0];
+
+    for(let i=0; i<prices.length; i++) {
+        let cost = prices[i] - minimum;
+        profit = Math.max(profit, cost);
+        minimum = Math.min(minimum, prices[i]);
+    }
+    return profit;
+};
