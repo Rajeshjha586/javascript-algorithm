852ba3bbeec4b8a65d263801a4c6fb41f36d1fe5
Rajesh Jha <rajeshjha@Rajeshs-MacBook-Air.local>
2025-03-05 00:02:27 +0530

    feat: Add leetcode problem best time to buy and sell stock kadanes algorithm

--- Arrays/Leetcode_121/bestTimeToBuyAndSell.js
+++ Arrays/Leetcode_121/bestTimeToBuyAndSell.js
@@ -4,7 +4,7 @@ var maxProfit = function(prices) {
     let profit = 0;
     let minimum = prices[0];
 
-    for(let i=0; i<prices.length; i++) {
+    for(let i=1; i<prices.length; i++) {
         let cost = prices[i] - minimum;
         profit = Math.max(profit, cost);
         minimum = Math.min(minimum, prices[i]);
