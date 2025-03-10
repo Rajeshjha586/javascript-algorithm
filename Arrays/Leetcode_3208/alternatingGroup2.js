0191867149a408fdf9f86b468c6a74f2c6946a22
Rajesh Jha <jharajesh035@gmail.com>
2025-03-11 00:39:14 +0530

    feat: Update alternating group 2 problem solution

--- Arrays/Leetcode_3208/alternatingGroup2.js
+++ Arrays/Leetcode_3208/alternatingGroup2.js
@@ -1,19 +1,23 @@
 // https://leetcode.com/problems/alternating-groups-ii/description/
 
 var numberOfAlternatingGroups = function(colors, k) {
-    colors.push(...colors.slice(0, k - 1)); 
+    let n = colors.length; 
     let count = 0;
     let left = 0;
     
-    for (let right = 0; right < colors.length; right++) {
-        if (right > 0 && colors[right] === colors[right - 1]) {
+    for (let right = 1; right < (n + k - 1); right++) {
+        if (colors[right % n] == colors[(right - 1) % n]) {
             left = right;  
         }
         
-        if (right - left + 1 >= k) {
+        if (right - left + 1 == k) {
             count++;  
+            left++;
         }
     }
     
     return count;
 };
+
+// Time Complexity:- O(N*K)
+// Space Complexity:- O(1)
