e78cd6cbeccc80cc0c193db705314ecb29c31eeb
Rajesh Jha <jharajesh035@gmail.com>
2025-03-17 00:49:10 +0530

    feat: Add minimum time to repair cars problem solution

---
+++ Arrays/Leetcode_2594/minimumTimeToRepairCars_BinarySearch.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/minimum-time-to-repair-cars/description/
+
+function repairCars(ranks, cars) {
+  let left = Math.min(...ranks);
+  let right = Math.max(...ranks) * cars * cars;
+  let ans = 0;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (isCarsRepaired(mid, ranks, cars)) {
+      ans = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return ans;
+}
+
+function isCarsRepaired(time, ranks, carsToBeRepaired) {
+  let carsRepaired = 0;
+  for (let rank of ranks) {
+    carsRepaired += Math.floor(Math.sqrt(time / rank));
+    if (carsRepaired >= carsToBeRepaired) return true;
+  }
+  return false;
+}
+
+// Time Complexity:- O(log(max * cars*2 - min)) + O(N);
+// Space complexity:- O(1)
