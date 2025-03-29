d483deef6eddab8a2cf009a9bf7a6d026340cbc3
Rajesh Jha <jharajesh035@gmail.com>
2025-03-30 00:01:24 +0530

    feat: Add 3 sum problem solution using two pointer

---
+++ two_pointers/running_from_both_ends_of_an_array/leetcode_15_3Sum/threeSum.js
@@ -0,0 +1,88 @@
+// https://leetcode.com/problems/3sum/description/
+
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  let result = [];
+  let target = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        result.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+
+        while (left < right && nums[left] === nums[left - 1]) {
+          left++;
+        }
+
+        while (left < right && nums[right] === nums[right + 1]) {
+          right--;
+        }
+      }
+    }
+  }
+
+  return result;
+};
+
+// Time Complexity:- O(NlogN + N2)
+// Space Complexity: O(1)
+
+// var getHash = function (x, y, z) {
+//   let hash = x;
+//   hash *= 100000;
+//   hash += y;
+//   hash *= 100000;
+//   hash += z;
+
+//   return hash;
+// };
+
+// var threeSum = function (nums) {
+//   nums.sort((a, b) => a - b);
+
+//   let result = [];
+
+//   let target = 0;
+//   const set = new Set();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let newTarget = target - nums[i];
+//     let left = i + 1;
+//     let right = nums.length - 1;
+//     while (left < right) {
+//       if (nums[left] + nums[right] < newTarget) {
+//         left++;
+//       } else if (nums[left] + nums[right] > newTarget) {
+//         right--;
+//       } else {
+//         const hash = getHash(nums[i], nums[left], nums[right]);
+
+//         if (!set.has(hash)) {
+//           result.push([nums[i], nums[left], nums[right]]);
+//           set.add(hash);
+//         }
+//         left++;
+//         right--;
+//       }
+//     }
+//   }
+
+//   return result;
+// };
+
+// Time Complexity:- O(NlogN + N2)
+// Space Complexity: O(N)
